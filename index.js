e08f940115768ed15cc1467189378362b0fc35c8
Bjarke Vad <bva@insilico.dk>
2016-06-16 15:01:48 +0200

    Performance

--- index.js
+++ index.js
@@ -25,201 +25,195 @@ var passed = true;
  * Wrap critical Flow exception into default Error json format
  */
 function fatalError(stderr) {
-  return {
-    errors: [{
-      message: [{
-        path: '',
-        code: 0,
-        line: 0,
-        start: 0,
-        descr: stderr
-      }]
-    }]
-  };
+    return {
+        errors: [{
+                    message: [{
+                                 path: '',
+                                     code: 0,
+                                     line: 0,
+                                     start: 0,
+                                     descr: stderr
+                             }]
+                }]
+    };
 }
 
 function optsToArgs(opts) {
-  var args = [];
+    var args = [];
 
-  if (opts.all) {
-    args.push('--all');
-  }
-  if (opts.weak) {
-    args.push('--weak');
-  }
-  if (opts.declarations) {
-    args.push('--lib', opts.declarations);
-  }
+    if (opts.all) {
+        args.push('--all');
+    }
+    if (opts.weak) {
+        args.push('--weak');
+    }
+    if (opts.declarations) {
+        args.push('--lib', opts.declarations);
+    }
 
-  return args;
+    return args;
 }
 
 function getFlowBin() {
-    return process.env.FLOW_BIN || flowBin;
+    var res = process.env.FLOW_BIN || flowBin;
+    console.log('Using flow found in: ' + res);
+    return res;
 }
 
-function executeFlow(_path, options) {
-  var deferred = Q.defer();
+function executeFlow (options) {
+    console.log('Executing flow in: '+ process.cwd());
+    var deferred = Q.defer();
 
-  var opts = optsToArgs(options);
+    var opts = optsToArgs(options);
 
-  var command = opts.length || options.killFlow ? (() => {
-    servers.push(path.dirname(_path));
-    //return 'check';
-    return '';
-  })() : 'status';
-
-  var args = [
-    command,
-    ...opts,
-    '/' + path.relative('/', _path),
-    '--json'
-  ];
-
-  var stream = childProcess.spawn(getFlowBin(), args);
-
-  stream.stdout.on('data', data => {
-    var parsed;
+    var stream;
     try {
-      parsed = JSON.parse(data.toString());
+        stream = childProcess.spawn(getFlowBin(), ['--json']);
+    } catch(e) {
+        console.log(e);
     }
-    catch(e) {
-      parsed = fatalError(data.toString());
-    }
-    var result = {};
 
-    // loop through errors in file
-    result.errors = parsed.errors.filter(function (error) {
-      let isCurrentFile = error.message[0].path === _path;
-      let generalError = (/(Fatal)/.test(error.message[0].descr));
+    stream.stdout.on('data', data => {
+        var parsed;
+        try {
+            parsed = JSON.parse(data.toString());
+        }
+        catch(e) {
+            parsed = fatalError(data.toString());
+        }
 
-      return isCurrentFile || generalError;
+        var result = {};
+
+        // loop through errors in file
+        result.errors = parsed.errors.filter(function (error) {
+            //let isCurrentFile = error.message[0].path; //=== _path;
+            //console.log(error.message[0].descr);
+            console.log(error.message[0].descr);
+
+            return true; // || isCurrentFile;
+        });
+
+        console.log(parsed.errors);
+
+        if (result.errors.length) {
+            passed = false;
+
+            var reporter = typeof options.reporter === 'undefined' ?
+                stylishReporter : options.reporter.reporter;
+
+           reporter(flowToJshint(result));
+                deferred.reject(new gutil.PluginError('gulp-flow', 'Flow failed'));
+
+            //if (options.abort) {
+                //deferred.reject(new gutil.PluginError('gulp-flow', 'Flow failed'));
+            //}
+            //else {
+                //deferred.resolve();
+            //}
+        }
+        else {
+            deferred.resolve();
+        }
     });
 
-    if (result.errors.length) {
-      passed = false;
-
-      var reporter = typeof options.reporter === 'undefined' ?
-        stylishReporter : options.reporter.reporter;
-
-      reporter(flowToJshint(result));
-
-      if (options.abort) {
-        deferred.reject(new gutil.PluginError('gulp-flow', 'Flow failed'));
-      }
-      else {
-        deferred.resolve();
-      }
-    }
-    else {
-      deferred.resolve();
-    }
-  });
-
-  return deferred.promise;
+    return deferred.promise;
 }
 
 function checkFlowConfigExist() {
-  var deferred = Q.defer();
-  var config = path.join(process.cwd(), '.flowconfig');
-  fs.exists(config, function(exists) {
-    if (exists) {
-      deferred.resolve();
-    }
-    else {
-      deferred.reject('Missing .flowconfig in the current working directory.');
-    }
-  });
-  return deferred.promise;
+    var deferred = Q.defer();
+    var config = path.join(process.cwd(), '.flowconfig');
+    fs.exists(config, function(exists) {
+        if (exists) {
+            deferred.resolve();
+        }
+        else {
+            deferred.reject('Missing .flowconfig in the current working directory.');
+        }
+    });
+    return deferred.promise;
 }
 
 function hasJsxPragma(contents) {
-  return /@flow\b/ig
-    .test(contents);
+    return /@flow\b/ig
+        .test(contents);
 }
 
 function isFileSuitable(file) {
-  var deferred = Q.defer();
-  if (file.isNull()) {
-    deferred.reject();
-  }
-  else if (file.isStream()) {
-    deferred.reject(new gutil.PluginError('gulp-flow', 'Stream content is not supported'));
-  }
-  else if (file.isBuffer()) {
+    var deferred = Q.defer();
     deferred.resolve();
-  }
-  else {
-    deferred.reject();
-  }
-  return deferred.promise;
+    return deferred.promise;
 }
 
 function killServers() {
-  var defers = servers.map(function(_path) {
-    var deferred = Q.defer();
-    childProcess.execFile(getFlowBin(), ['stop'], {
-      cwd: _path
-    }, deferred.resolve);
-    return deferred;
-  });
-  return Q.all(defers);
+    var defers = servers.map(function(_path) {
+        var deferred = Q.defer();
+        childProcess.execFile(getFlowBin(), ['stop'], {
+            cwd: _path
+        }, deferred.resolve);
+        return deferred;
+    });
+    return Q.all(defers);
 }
 
+var firstRun = true;
+
 module.exports = function (options={}) {
-  options.beep = typeof options.beep !== 'undefined' ? options.beep : true;
+    options.beep = typeof options.beep !== 'undefined' ? options.beep : true;
 
-  function Flow(file, enc, callback) {
-
-    var _continue = () => {
-      this.push(file);
-      callback();
-    };
-
-    isFileSuitable(file).then(() => {
-      var hasPragma = hasJsxPragma(file.contents.toString());
-      if (options.all || hasPragma) {
-        checkFlowConfigExist().then(() => {
-          executeFlow(file.path, options).then(_continue, err => {
-            this.emit('error', err);
+    function Flow(file, enc, callback) {
+        var _continue = () => {
+            this.push(file);
             callback();
-          });
-        }, msg => {
-          console.log(logSymbols.warning + ' ' + msg);
-          _continue();
+        };
+
+        isFileSuitable(file).then(() => {
+            if (firstRun) {
+                checkFlowConfigExist().then(() => {
+                    executeFlow(file.path, options).then(() => {
+                        firstRun = false;
+                        _continue()}, err => {
+                            this.emit('error', err);
+                            firstRun = false;
+                            callback();
+                        });
+                }, msg => {
+                    console.log(logSymbols.warning + ' ' + msg);
+                    firstRun = false;
+                    _continue();
+                });
+            } else {
+                _continue();
+            }
+        }, err => {
+            if (err) {
+                this.emit('error', err);
+            }
+            callback();
         });
-      } else {
-        _continue();
-      }
-    }, err => {
-      if (err) {
-        this.emit('error', err);
-      }
-      callback();
+    }
+
+
+    return through.obj(Flow, function () {
+        var end = () => {
+            this.emit('end');
+            passed = true;
+        };
+
+        if (passed) {
+            console.log(logSymbols.success + ' Flow has found 0 errors');
+        } else if (options.beep) {
+            gutil.beep();
+        }
+
+        if (options.killFlow) {
+            if (servers.length) {
+                killServers().done(end);
+            }
+            else {
+                end();
+            }
+        } else {
+            end();
+        }
     });
-  }
-
-  return through.obj(Flow, function () {
-    var end = () => {
-      this.emit('end');
-      passed = true;
-    };
-
-    if (passed) {
-      console.log(logSymbols.success + ' Flow has found 0 errors');
-    } else if (options.beep) {
-      gutil.beep();
-    }
-
-    if (options.killFlow) {
-      if (servers.length) {
-        killServers().done(end);
-      }
-      else {
-        end();
-      }
-    } else {
-      end();
-    }
-  });
 };
